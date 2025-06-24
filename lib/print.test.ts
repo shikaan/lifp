da6fe5be23991b74aa25901dc1479723d6bd780f
Manuel Spagnolo <manuel.spagnolo@proton.me>
2025-06-24 09:22:01 +0200

    chore: lint

--- lib/print.test.ts
+++ lib/print.test.ts
@@ -4,36 +4,36 @@ import { print } from "./print.js";
 import { ASTNodeType, type Expression } from "./types.js";
 
 test("atoms", () => {
-	const tests: [Expression, string][] = [
-		[n(ASTNodeType.NIL, null), "nil"],
-		[n(ASTNodeType.NUMBER, 1), "1"],
-		[n(ASTNodeType.SYMBOL, "asd"), "asd"],
-		[n(ASTNodeType.STRING, "asd"), '"asd"'],
-		[n(ASTNodeType.KEYWORD, ":asd"), ":asd"],
-		[n(ASTNodeType.BOOLEAN, true), "true"],
-	];
+  const tests: [Expression, string][] = [
+    [n(ASTNodeType.NIL, null), "nil"],
+    [n(ASTNodeType.NUMBER, 1), "1"],
+    [n(ASTNodeType.SYMBOL, "asd"), "asd"],
+    [n(ASTNodeType.STRING, "asd"), '"asd"'],
+    [n(ASTNodeType.KEYWORD, ":asd"), ":asd"],
+    [n(ASTNodeType.BOOLEAN, true), "true"],
+  ];
 
-	for (const [input, expected] of tests) {
-		expect(print(input), expected).toEqual(expected);
-	}
+  for (const [input, expected] of tests) {
+    expect(print(input), expected).toEqual(expected);
+  }
 });
 
 test("lists", () => {
-	const tests: [Expression, string][] = [
-		[l([n(ASTNodeType.NIL, null)]), "(nil)"],
-		[
-			l([
-				n(ASTNodeType.NIL, null),
-				n(ASTNodeType.NUMBER, 1),
-				n(ASTNodeType.BOOLEAN, true),
-				n(ASTNodeType.KEYWORD, ":lol"),
-			]),
-			"(nil 1 true :lol)",
-		],
-		[l([n(ASTNodeType.NIL, null), l([n(ASTNodeType.NUMBER, 1)])]), "(nil (1))"],
-	];
+  const tests: [Expression, string][] = [
+    [l([n(ASTNodeType.NIL, null)]), "(nil)"],
+    [
+      l([
+        n(ASTNodeType.NIL, null),
+        n(ASTNodeType.NUMBER, 1),
+        n(ASTNodeType.BOOLEAN, true),
+        n(ASTNodeType.KEYWORD, ":lol"),
+      ]),
+      "(nil 1 true :lol)",
+    ],
+    [l([n(ASTNodeType.NIL, null), l([n(ASTNodeType.NUMBER, 1)])]), "(nil (1))"],
+  ];
 
-	for (const [input, expected] of tests) {
-		expect(print(input), expected).toEqual(expected);
-	}
+  for (const [input, expected] of tests) {
+    expect(print(input), expected).toEqual(expected);
+  }
 });
