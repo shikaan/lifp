289e635fc6d504b24d646ee323f13aa5ac91814b
Manuel Spagnolo <manuel.spagnolo@proton.me>
2025-09-02 15:21:49 +0200

    fix: make stringCopy act like strlcpy

--- lib/string.h
+++ lib/string.h
@@ -5,12 +5,14 @@
 
 // This is a portable re-implementation of strlcpy
 static inline size_t stringCopy(char *dst, const char *src, size_t size) {
-  size_t srclen = strlen(src);
+  size_t src_length = strlen(src);
 
-  if (size > 0) {
-    size_t copylen = (srclen >= size) ? size - 1 : srclen;
-    memcpy(dst, src, copylen);
-    dst[copylen] = '\0';
+  if (size != 0) {
+    size_t dst_length = (src_length >= size) ? size - 1 : src_length;
+    if (dst_length > 0) {
+      memcpy(dst, src, dst_length);
+    }
+    dst[dst_length] = '\0';
   }
-  return srclen;
+  return src_length;
 }
